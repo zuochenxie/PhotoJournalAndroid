2094a5fba47fefb059ff2edcf0280c89ae0db22a
boluan <bo_luan@umail.ucsb.edu>
2017-03-14 11:02:29 -0700

    JournalManager and JournalAdapter

---
+++ PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/JournalManager.java
@@ -0,0 +1,45 @@
+package edu.ucsb.cs.cs185.photojournal.photojournal;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by rexluan on 3/14/17.
+ */
+public class JournalManager {
+    private static JournalManager ourInstance = new JournalManager();
+    public static JournalManager getInstance() {
+        return ourInstance;
+    }
+
+    public static final List<Journal> ITEMS = new ArrayList<Journal>();
+    public static JournalAdapter adapter = new JournalAdapter();
+
+    private JournalManager() {
+    }
+
+    public static void addItem(Journal item){
+        ITEMS.add(item);
+        adapter.notifyDataSetChanged();
+    }
+
+    public static void removeItem(Journal item){
+        ITEMS.remove(item);
+        adapter.notifyDataSetChanged();
+    }
+
+    public static class Journal{
+        public Bitmap image;
+        public String caption;
+        public Date date;
+
+        public Journal(Bitmap _image){
+            image = _image;
+            caption = "";
+            date = new Date();
+        }
+    }
+}
