9126cc6b679eb072bd9e7146ff5a226a77366629
Joe Song <joesong@Joes-MacBook-Pro.local>
2017-03-18 17:53:22 -0700

    created list_image.xml and implemented ImageAdapter and ImageManager

--- PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/ImageManager.java
+++ PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/ImageManager.java
@@ -1,8 +1,18 @@
 package edu.ucsb.cs.cs185.photojournal.photojournal;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+
 /**
  * Created by joesong on 3/18/17.
  */
 
 public class ImageManager {
+    public Uri uri;
+    public Bitmap bitmap;
+
+    public ImageManager(Uri uri, Bitmap bitmap){
+        this.uri = uri;
+        this.bitmap = bitmap;
+    }
 }
