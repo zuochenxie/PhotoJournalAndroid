988c7eee0d93a3717a043554a412b553e4c95c88
Joe Song <joesong@Joes-MacBook-Pro.local>
2017-03-18 17:06:23 -0700

    Started PhotoView class and created photo_view_drawer.xml

--- PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/PhotoView.java
+++ PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/PhotoView.java
@@ -1,9 +1,21 @@
 package edu.ucsb.cs.cs185.photojournal.photojournal;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
 /**
  * Created by joesong on 3/18/17.
  */
 
-public class PhotoView {
+public class PhotoView extends AppCompatActivity{
+    ImageView image;
+    public int imgIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photo_view);
+    }
 
 }
