964ea5466472575fcbc1fea181e8f037c7d8882d
Joe Song <joesong@Joes-MacBook-Pro.local>
2017-03-18 18:38:48 -0700

    Renamed a file and can now add photos from gallery into list view

--- PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/ListFragment.java
+++ PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/ListFragment.java
@@ -1,13 +1,58 @@
 package edu.ucsb.cs.cs185.photojournal.photojournal;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import static edu.ucsb.cs.cs185.photojournal.photojournal.MainActivity.images;
+
 /**
  * Created by joesong on 3/18/17.
  */
 
-public class ListFragment {
+public class ListFragment extends Fragment{
     public ImageAdapter adapter;
 
     public ListFragment(){
 
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume(){
+        adapter.notifyDataSetChanged();
+        super.onResume();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.list_fragment, container, false);
+
+        final ListView imageView = (ListView) rootView.findViewById(R.id.image_list);
+        adapter = new ImageAdapter(getActivity().getApplicationContext(), images);
+        imageView.setAdapter(adapter);
+
+        imageView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int x, long y){
+                Intent intent = new Intent(getActivity(), PhotoViewActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("INDEX", x);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
+    }
+
 }
--- PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/PhotoViewActivity.java
+++ PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/PhotoViewActivity.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
  * Created by joesong on 3/18/17.
  */
 
-public class PhotoView extends AppCompatActivity{
+public class PhotoViewActivity extends AppCompatActivity{
     ImageView image;
     public int imgIndex;
 
