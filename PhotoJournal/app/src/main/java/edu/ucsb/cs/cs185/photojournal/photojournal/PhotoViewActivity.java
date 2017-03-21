ac4943ffe303620c0f0cb31c2b80a520b4b07156
Joe Song <joesong@Joes-MacBook-Pro.local>
2017-03-20 23:52:43 -0700

    Added social media icons to photo view

--- PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/PhotoViewActivity.java
+++ PhotoJournal/app/src/main/java/edu/ucsb/cs/cs185/photojournal/photojournal/PhotoViewActivity.java
@@ -79,6 +79,12 @@ public class PhotoViewActivity extends AppCompatActivity{
                     date.setText(s);
                 }
             }
+
+            public void onSwipeTop() {
+            }
+            public void onSwipeBottom() {
+            }
+
         });
     }
 
